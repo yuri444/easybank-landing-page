9b7469e17661bdc8f7c71e822f9444bb21b5d0f6
yuri444 <ybarros345@gmail.com>
2021-05-20 23:45:29 +0200

    done

---
+++ app/js/script.js
@@ -0,0 +1,42 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+  console.log('click hamburger');
+
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+    
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+
+  }  
+});
+
+
+// const menuBtn = document.querySelector(".nav__burgermenu");
+// let menuOpen = false;
+
+// menuBtn.addEventListener("click", () => {
+//   if (!menuOpen) {
+//     menuBtn.classList.add("open");
+//     menuOpen = true;
+//   } else {
+//     menuBtn.classList.remove("open");
+//     menuOpen = false;
+//   }
+// });
